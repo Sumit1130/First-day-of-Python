23dbe338ae1417d5e001e4c2b25cccccfe0841ae
sumit nepal <anepal775@gmail.com>
2025-08-05 16:29:14 +0400

    first day

---
+++ days_1.py
@@ -0,0 +1,133 @@
+# print("hello world")
+# name = "sumit nepal"
+# print(f"my name is {name}")
+# name = "sumit nepal"
+# age = 22
+# roll_no = 1
+# print(f"my name is: {name} ")
+# print(f"my age is : {age}")
+# print(f"my roll no is : {roll_no}")
+# name = "sumit nepal"
+# age = 23
+# number = 23.4
+
+# print(type(name))
+# print(type(age))
+# print(type(number))
+
+# print sum
+# a = 2
+# b = 3
+# sum = (a+b)
+# print(f"{sum}")
+
+# a = 2
+# b = 3
+# diff = (a-b)
+# print(f"{diff}")
+
+# arithmetic operators
+# a = 27
+# b = 3
+
+# print(a + b)
+# print(a - b)
+# print(a / b)
+# print(a % b)#module / remainder
+# print(a*b)
+# print(a**b)#a^b
+
+# relational operators
+# a = 23
+# b = 33
+# print(a == b)
+# print(a != b)
+# print(a < b)
+# print(a > b)
+# print(a >= b)
+# print(a <= b)
+
+# assignment operators
+# a = 10
+# a += 10
+# print(f"your num: {a}")
+# a = 10
+# a -= 10
+# print(f"your num: {a}")
+
+# a = 10
+# a *= 10
+# print(f"your num: {a}")
+
+# a = 10
+# a /= 10
+# print(f"your num: {a}")
+
+# a = 10
+# a %= 10
+# print(f"your num: {a}")
+
+# a = 10
+# a **= 10
+# print(f"your num: {a}")
+
+# logical operators
+# print(not True)
+# print(not False)
+# a = 50
+# b = 30
+# print(not a > b)
+
+# value1 = True
+# value2 = True
+# print(value1 and value2)#need both value correct
+
+# a = 30 
+# b = 40 
+# print((a == b) or (a < b))#if one value correct give true if both value false give false
+
+
+
+
+# type conversion
+# a = 10 
+# b = 20.25
+# print(a + b)#automatic conversion
+
+# type casting
+# a = int("2")
+# b = 2.4
+# print(a + b)
+
+
+# input in python
+# Name = input("Enter your name: ")
+# Age = input("Enter your age: ")
+# salary = float(input("Enter your salary: "))
+# print(f"Your name is : {Name}")
+# print(f"your age is: {Age}  ")
+# print(f"your salry is: {salary}")
+
+
+# # practice question
+
+# first = int(input("Enter your first value: "))
+# second = int(input("Enter your second value: "))
+# print(f" sum = {first + second}")
+
+
+# side1 = float(input("Enter your side1 value: "))
+# side2 = float(input("Enter your side2 value: "))
+# square_area = (side1 * side2)
+# print(f"your squre area= {square_area} ")
+
+
+# number1 = float(input("Enter your number1: "))
+# number2 = float(input("Enter your number2: "))
+# Average = (number1 + number2)/2
+# print(f"your average is = {Average}")
+
+
+# a = float(input("Enter your number1: "))
+# b = float(input("Enter your number2: "))
+# print(f"{a >= b}")
